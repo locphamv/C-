1ab5543a260d2748f756d9f74a6ccb6751c75aa6
Phạm Văn Lộc <devlocphamv@gmail.com>
2025-12-19 20:35:27 +0700

    chore: Test contribution graph

--- Final_exam/Ex1.cpp
+++ Final_exam/Ex1.cpp
@@ -6,15 +6,15 @@ void traVeKetQua(string a, string b)
 {
     if (a == "KEO" && b == "BAO" || a == "BUA" && b == "KEO" || a == "BAO" && b == "BUA")
     {
-        cout << "A WON" << "(" << a << "-" << b << ")";
+        cout << "A WON " << "(" << a << "-" << b << ")";
     }
     else if (a == "KEO" && b == "BUA" || a == "BUA" && b == "BAO" || a == "BAO" && b == "KEO")
     {
-        cout << "B WON" << "(" << a << "-" << b << ")";
+        cout << "B WON " << "(" << a << "-" << b << ")";
     }
     else
     {
-        cout << "DRAW" << "(" << a << "-" << b << ")";
+        cout << "DRAW " << "(" << a << "-" << b << ")";
     }
 }
 
