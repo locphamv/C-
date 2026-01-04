e11116c247ab7434f60b1df6fc4158a6cef31706
Phạm Văn Lộc <devlocphamv@gmail.com>
2026-01-04 20:22:49 +0700

    Solve LC0412 Fizz Buzz

---
+++ Leatcode/412.fizz-buzz.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=412 lang=cpp
+ *
+ * [412] Fizz Buzz
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    vector<string> fizzBuzz(int n)
+    {
+        vector<string> res(n);
+        for (int i = 0; i < n; i++)
+        {
+            if ((i + 1) % 15 == 0)
+            {
+                res[i] = "FizzBuzz";
+            }
+            else if ((i + 1) % 3 == 0)
+            {
+                res[i] = "Fizz";
+            }
+            else if ((i + 1) % 5 == 0)
+            {
+                res[i] = "Buzz";
+            }
+            else
+            {
+                res[i] = to_string(i + 1);
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
