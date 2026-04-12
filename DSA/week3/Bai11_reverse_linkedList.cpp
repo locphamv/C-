31c8b55f1b99b5243eb0902469f1fae6e90138ba
Phạm Văn Lộc <devlocphamv@gmail.com>
2026-04-12 14:23:44 +0700

    Linked List

---
+++ DSA/week3/Bai11_reverse_linkedList.cpp
@@ -0,0 +1,86 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct NODE {
+    int info;
+    NODE* pNext;
+};
+
+
+struct LIST {
+    NODE* pHead;
+    NODE* pTail;
+};
+
+void CreateEmptyList(LIST &L) {
+    L.pHead = NULL;
+    L.pTail = NULL;
+}
+
+NODE* CreateNode(int x) {
+    NODE* p = new NODE;
+    if (p == NULL)
+        exit(1);
+    p->info = x;
+    p->pNext = NULL;
+    return p;
+}
+
+void InsertTail(LIST &L, NODE* p) {
+    if (L.pHead == NULL) {
+        L.pTail = p;
+        L.pHead = L.pTail;
+    }
+    else {
+        L.pTail->pNext = p;
+        L.pTail = p;
+    }
+}
+
+void PrintList(LIST L)
+{
+    NODE *p = L.pHead;
+    while (p != NULL)
+    {
+        cout << p->info << " ";
+        p = p->pNext;
+    }
+}
+
+void ReverseLL(LIST &);
+
+void ReverseLL(LIST &L)
+{
+    NODE*prev=NULL;
+    NODE*p=L.pHead;
+
+    L.pTail=L.pHead;
+
+    while(p)
+    {
+        NODE*tmp=p->pNext;
+        p->pNext=prev;
+        prev=p;
+        p=tmp;
+    }
+L.pHead=prev; 
+}
+
+
+
+
+int main()
+{
+    LIST L;
+    CreateEmptyList(L);
+    int n; cin >> n;
+    int x;
+    for (int i = 0; i < n; i++){
+        cin >> x;
+        InsertTail(L, CreateNode(x));
+    }
+    ReverseLL(L);
+    PrintList(L);
+    return 0;
+}
