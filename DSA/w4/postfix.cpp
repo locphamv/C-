3c013cc82814d4849cf6918692b38bf022cc044f
Phạm Văn Lộc <devlocphamv@gmail.com>
2026-04-24 20:56:07 +0700

    redo

---
+++ DSA/w4/postfix.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isNumber(const string &s)
+{
+    if (s.empty())
+        return false;
+    int i = 0;
+    if (s[0] == '-')
+    {
+        if (s.size() == 1)
+            return false;
+        i = 1;
+    }
+    for (; i < (int)s.size(); i++)
+    {
+        if (!isdigit(s[i]))
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string line;
+    getline(cin, line);
+
+    stringstream ss(line);
+    string token;
+    stack<long long> st;
+
+    while (ss >> token)
+    {
+        if (isNumber(token))
+        {
+            st.push(stoll(token));
+        }
+        else if (token == "+" || token == "-" || token == "x" || token == ":")
+        {
+            if (st.size() < 2)
+            {
+                cout << "KHONG HOP LE";
+                return 0;
+            }
+
+            long long b = st.top();
+            st.pop();
+            long long a = st.top();
+            st.pop();
+
+            long long res;
+            if (token == "+")
+                res = a + b;
+            else if (token == "-")
+                res = a - b;
+            else if (token == "x")
+                res = a * b;
+            else
+            {
+                if (b == 0)
+                {
+                    cout << "KHONG HOP LE";
+                    return 0;
+                }
+                res = a / b;
+            }
+
+            st.push(res);
+        }
+        else
+        {
+            cout << "KHONG HOP LE";
+            return 0;
+        }
+    }
+
+    if (st.size() != 1)
+    {
+        cout << "KHONG HOP LE";
+    }
+    else
+    {
+        cout << st.top();
+    }
+
+    return 0;
+}
