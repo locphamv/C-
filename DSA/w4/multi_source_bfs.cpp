a50bf0c3d5ba6a5228fda32f6a01c1a0920489cd
Phạm Văn Lộc <devlocphamv@gmail.com>
2026-04-25 10:58:50 +0700

    Clarify queue purpose in multi_source_bfs.cpp
    Updated comment to clarify the purpose of the queue.

--- DSA/w4/multi_source_bfs.cpp
+++ DSA/w4/multi_source_bfs.cpp
@@ -1,5 +1,5 @@
 // MULTI SOURCE BFS:
-//queue chứ cặp toạ độ 
+//queue chứ cặp toạ độ gốc
 // đưa các source vào trước gắn count= 0
 // 2 mảng dx dy và 4 vòng for để xét 4 hướng (0=<row<m && 0<=col<n)
 // gặp x cần tìm thì gắn count= count trước đó +1 và đưa vào queue chờ xét 4 hướng.
