dbcbc26b8fab50c390d97aa839f460a7f9b31d94
Phạm Văn Lộc <devlocphamv@gmail.com>
2026-05-16 10:56:10 +0700

    feat: implement solution for recovering a binary search tree

---
+++ Leatcode/99.recover-binary-search-tree.cpp
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode id=99 lang=cpp
+ *
+ * [99] Recover Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution
+{
+  public:
+    TreeNode *first = nullptr;
+    TreeNode *second = nullptr;
+    TreeNode *prev = nullptr;
+
+    void inOrder(TreeNode *root)
+    {
+        if (root == nullptr)
+        {
+            return;
+        }
+        inOrder(root->left);
+        if (prev != nullptr && prev->val > root->val)
+        {
+            if (first == nullptr)
+            {
+                first = prev;
+            }
+            second = root;
+        }
+        prev = root;
+        inOrder(root->right);
+    }
+
+    void recoverTree(TreeNode *root)
+    {
+        inOrder(root);
+        if (first != nullptr && second != nullptr)
+        {
+            swap(first->val, second->val);
+        }
+    }
+};
+// @lc code=end
