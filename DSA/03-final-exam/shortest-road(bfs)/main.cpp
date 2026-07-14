18932934292c32a035cdb5399604b82caa6b94b3
Phạm Văn Lộc <devlocphamv@gmail.com>
2026-07-14 20:40:44 +0700

    feat: implement BFS algorithm to find shortest path in an undirected graph

---
+++ DSA/03-final-exam/shortest-road(bfs)/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> adj(n + 1);
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    int s, t;
+    cin >> s >> t;
+    vector<int> dist(n + 1, -1);
+    queue<int> q;
+    dist[s] = 0;
+    q.push(s);
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        for (int v : adj[u]) {
+            if (dist[v] == -1) {
+                dist[v] = dist[u] + 1;
+                q.push(v);
+            }
+        }
+    }
+    cout << dist[t];
+
+    return 0;
+}
